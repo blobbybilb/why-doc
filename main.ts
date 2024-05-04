70278911ffcbdeb8c767958d535740fb4bba4e2d
blobbybilb <notablob@outlook.com>
2024-05-03 19:44:22 -0700

    save

--- main.ts
+++ main.ts
@@ -1,23 +1,20 @@
-import { Hono } from "https://deno.land/x/hono@v3.2.5/mod.ts"
-import { serve } from "https://deno.land/std@0.190.0/http/server.ts"
+import { Hono } from "https://deno.land/x/hono@v3.2.5/mod.ts";
+import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 // import { genDocs, WhyDoc } from "./doc.ts"
 import {
   genDocs,
   WhyDoc,
-} from "https://raw.githubusercontent.com/blobbybilb/why-doc/main/doc.ts"
-import { serveStatic } from "https://deno.land/x/hono@v3.2.5/middleware.ts"
+} from "https://raw.githubusercontent.com/blobbybilb/why-doc/main/doc.ts";
+import { serveStatic } from "https://deno.land/x/hono@v3.2.5/middleware.ts";
 
 const doc = WhyDoc({
   title: "Test API Docs",
   basePath: "/api", // optional
-})
+});
 
-const app = new Hono()
+const app = new Hono();
 
-app.get(
-  doc("/test", "a simple test"),
-  (c) => c.text("test"),
-)
+app.get(doc("/test", "a simple test"), (c) => c.text("test"));
 
 app.post(
   doc("/abc", "", {
@@ -25,8 +22,8 @@ app.post(
       name: "string",
     },
   }),
-  (c) => c.text("Done!"),
-)
+  (c) => c.text("Done!")
+);
 
 app.get(
   doc("/xyz", "returns the 'name' GET parameter", {
@@ -34,10 +31,12 @@ app.get(
       name: "string",
     },
   }),
-  (c) => c.text(c.req.query("name")!),
-)
+  (c) => c.text(c.req.query("name")!)
+);
 
-genDocs()
-app.use("/docs", serveStatic({ path: "./docs.html" }))
+genDocs();
+app.use("/docs", serveStatic({ path: "./docs.html" }));
 
-serve(app.fetch)
+app.get("/hi/:some/:thing", (c) => c.text(`Hello, ${c.req.param("key")}`));
+
+serve(app.fetch);
